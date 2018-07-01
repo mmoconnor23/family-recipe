857141b550e8304414386774ca14a1b66a74e31c
Melissa O'Connor <mmoconnor23@gmail.com>
2018-07-01 15:47:12 -0400

    feat(db): hook up to mongodb

--- src/services/recipeData.js
+++ src/services/recipeData.js
@@ -1,4 +1,7 @@
+/* eslint class-methods-use-this: 0 */
+
 import { sortBy } from 'lodash';
+import * as axios from 'axios';
 
 class RecipeData {
   constructor() {
@@ -26,101 +29,6 @@ class RecipeData {
         value: 'entree',
       },
     ];
-
-    // TODO: add familyId
-    this.recipes = [
-      {
-        title: 'Spinach and Artichoke Dip',
-        contributor: 'Melissa O\'Connor',
-        steps: ['Step 1', 'Step 2', 'Step 3'],
-        ingredients: ['Ingredient 1', 'Ingredient 2', 'Ingredient 3'],
-        pictures: ['img1', 'img2'],
-        categories: ['appetizer'],
-        isVegetarian: true,
-        id: '0',
-        description: 'This is a sample recipe. I will use it to test the styling of the display page.',
-      }, {
-        title: 'Cupcakes',
-        contributor: 'Melissa O\'Connor',
-        steps: ['Step 1', 'Step 2', 'Step 3'],
-        ingredients: ['Ingredient 1', 'Ingredient 2', 'Ingredient 3'],
-        pictures: ['img1', 'img2'],
-        categories: ['dessert'],
-        isVegetarian: true,
-        id: '1',
-        description: 'This is a sample recipe. I will use it to test the styling of the display page.',
-      }, {
-        title: 'Green (Creamed Spinach) Noodles',
-        contributor: 'Melissa O\'Connor',
-        steps: ['Step 1', 'Step 2', 'Step 3'],
-        ingredients: ['Ingredient 1', 'Ingredient 2', 'Ingredient 3'],
-        pictures: ['img1', 'img2'],
-        categories: ['entree', 'pasta'],
-        isVegetarian: true,
-        id: '2',
-        description: 'This is a sample recipe. I will use it to test the styling of the display page.',
-      }, {
-        title: 'Cauliflower Soup',
-        contributor: 'Melissa O\'Connor',
-        steps: ['Step 1', 'Step 2', 'Step 3'],
-        ingredients: ['Ingredient 1', 'Ingredient 2', 'Ingredient 3'],
-        pictures: ['img1', 'img2'],
-        categories: ['soup'],
-        isVegetarian: false,
-        id: '3',
-        description: 'This is a sample recipe. I will use it to test the styling of the display page.',
-      }, {
-        title: 'Mexican-Style Zucchini Casserole',
-        contributor: 'Melissa O\'Connor',
-        steps: ['Step 1', 'Step 2', 'Step 3'],
-        ingredients: ['Ingredient 1', 'Ingredient 2', 'Ingredient 3'],
-        pictures: ['img1', 'img2'],
-        categories: ['entree', 'casserole'],
-        isVegetarian: false,
-        id: '4',
-        description: 'This is a sample recipe. I will use it to test the styling of the display page.',
-      }, {
-        title: 'Tomato, Mozzarella & Pesto Sandwich',
-        contributor: 'Melissa O\'Connor',
-        steps: ['Step 1', 'Step 2', 'Step 3'],
-        ingredients: ['Ingredient 1', 'Ingredient 2', 'Ingredient 3'],
-        pictures: ['img1', 'img2'],
-        categories: ['entree', 'sandwich'],
-        isVegetarian: false,
-        id: '5',
-        description: 'This is a sample recipe. I will use it to test the styling of the display page.',
-      }, {
-        title: 'Grilled Cheese Sandwich',
-        contributor: 'Melissa O\'Connor',
-        steps: ['Step 1', 'Step 2', 'Step 3'],
-        ingredients: ['Ingredient 1', 'Ingredient 2', 'Ingredient 3'],
-        pictures: ['img1', 'img2'],
-        categories: ['entree', 'sandwich'],
-        isVegetarian: false,
-        id: '6',
-        description: 'This is a sample recipe. I will use it to test the styling of the display page.',
-      }, {
-        title: 'White Noodles',
-        contributor: 'Melissa O\'Connor',
-        steps: ['Step 1', 'Step 2', 'Step 3'],
-        ingredients: ['Ingredient 1', 'Ingredient 2', 'Ingredient 3'],
-        pictures: ['img1', 'img2'],
-        categories: ['entree', 'pasta'],
-        isVegetarian: false,
-        id: '7',
-        description: 'This is a sample recipe. I will use it to test the styling of the display page.',
-      }, {
-        title: 'Baked Ziti',
-        contributor: 'Melissa O\'Connor',
-        steps: ['Step 1', 'Step 2', 'Step 3'],
-        ingredients: ['Ingredient 1', 'Ingredient 2', 'Ingredient 3'],
-        pictures: ['img1', 'img2'],
-        categories: ['entree', 'pasta'],
-        isVegetarian: false,
-        id: '8',
-        description: 'This is a sample recipe. I will use it to test the styling of the display page.',
-      },
-    ];
   }
 
   getCategories() {
@@ -128,7 +36,9 @@ class RecipeData {
   }
 
   getRecipes() {
-    return Promise.resolve(sortBy(this.recipes, 'title'));
+    const url = 'https://vegapie-recipe-server.herokuapp.com/all';
+    // const url = 'http://localhost:8081/all';
+    return axios.get(url).then(response => sortBy(response.data, 'title'));
   }
 }
 
